b474248fac90c4f6cbfdf868a42efe932e7db8a9
Rhondoid <110504360+Rhondoid@users.noreply.github.com>
2023-02-22 22:58:04 -0700

    Navbar is functioning

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import './App.css';
-import About from './components/About.js';
-import Contact from './components/Contact.js';
-import Portfolio from './components/Portfolio.js';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import About from "./components/About.js";
+import Contact from "./components/Contact.js";
+import Portfolio from "./components/Portfolio.js";
+import Navbar from "./components/Navbar.js";
 
 // function App() {
 //   return (
@@ -13,24 +14,47 @@ import Portfolio from './components/Portfolio.js';
 //       <Portfolio/>
 
 //     </div>
-    
+
 //   );
 // }
 
 // export default App;
+// export default function App() {
+//   return (
+//     <div>
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/" element={<About />}>
+//           <Navbar/>
+//           <Route path= "/Contact" element={<Contact />} />
+//           <Route path="/Portfolio" element={<Portfolio />} />
+
+//         </Route>
+//       </Routes>
+//     </BrowserRouter>
+//       {/* <div className="Portfolio">
+//       <Navbar />
+//         </div> */}
+//     </div>
+//   );
+// }
 export default function App() {
   return (
-    <BrowserRouter>
+    <Router>
+      <Navbar />
       <Routes>
-        <Route path="/" element={<About />}>
-          <Route path= "/Contact" element={<Contact />} />
-          <Route path="/Portfolio" element={<Portfolio />} />
-          
-        </Route>
+        {/* <Route path='/Contact' exact element={Contact} /> */}
+        {/* <Route path='/About' element={About} /> */}
+        {/* <Route path='/Portfolio' element={Portfolio} /> */}
+        <Route path= "/Contact" element={<Contact />} />
+        <Route path="/Portfolio" element={<Portfolio />} />
+        {/* <Route path= "/Contact" element={<Contact />} /> */}
+        <Route path="/About" element={<About />} />
+        
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);

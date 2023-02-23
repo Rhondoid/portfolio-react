cef9114159f408e76eb9f527682a09e7daf18063
Rhondoid <110504360+Rhondoid@users.noreply.github.com>
2023-02-22 21:29:43 -0700

    portfolio.js component is working

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@
 import './App.css';
 import About from './components/About.js';
 import Contact from './components/Contact.js';
+import Portfolio from './components/Portfolio.js';
 
 function App() {
   return (
     <div className="Portfolio">
       <About />
       <Contact/>
+      <Portfolio/>
+
     </div>
     
   );
---
+++ src/components/Portfolio.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Portfolio() {
+  return (
+    <div>
+      Rhonda's Projects  are cool
+    </div>
+  )
+}

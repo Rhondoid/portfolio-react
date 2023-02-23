d6983e48bc7a9a11720ebabc51411c8150797a49
Rhondoid <110504360+Rhondoid@users.noreply.github.com>
2023-02-22 21:27:28 -0700

    contact.js component is working

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 
 import './App.css';
 import About from './components/About.js';
+import Contact from './components/Contact.js';
 
 function App() {
   return (
     <div className="Portfolio">
       <About />
+      <Contact/>
     </div>
+    
   );
 }
 
